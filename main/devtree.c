d685dc9fcab068af71d3e8e1392fbe4abf219678
Alex <15167344+frostmorn@users.noreply.github.com>
2026-07-01 15:16:06 +0300

    Basic devtree/device operations skelethon

--- main/devtree.c
+++ main/devtree.c
@@ -5,8 +5,10 @@ eos_dev_t eos_root_dev;
 
 void eos_devtree_init() {
   eos_root_dev = (eos_dev_t){.driver = &(eos_drivers[0]),
-                  .config = NULL,
-                  .state = NULL,
-                  .parent = NULL,
-                  .child = NULL};
+                             .config = NULL,
+                             .state = NULL,
+                             .parent = NULL,
+                             .child = NULL};
 }
+
+eos_dev_t *eos_devtree_root() { return &eos_root_dev; }
--- main/devtree.h
+++ main/devtree.h
@@ -14,7 +14,8 @@ typedef struct eos_dev_t eos_dev_t;
 struct eos_dev_t {
   // Driver to be used to control device
   eos_driver_t *driver;
-  // Static device config
+  // Static device config, further reconfiguring, should be
+  // maintained through ioctl provided routines
   void *config;
   // Device state maintained by driver
   void *state;
@@ -25,7 +26,18 @@ struct eos_dev_t {
   eos_dev_t *next;
 };
 
-extern eos_dev_t eos_root_dev;
-
 // Initializes device tree
 void eos_devtree_init();
+
+// Returns a pointer to root device
+eos_dev_t *eos_devtree_root();
+
+// Allocates a new device or returns NULL in case of error
+// and sets eos_errno value
+eos_dev_t *eos_dev_alloc();
+
+// Attaches device to EOS device tree
+eos_error_t eos_dev_attach(eos_dev_t *dev, eos_dev_t *dev_bus);
+
+// Detaches device and all it's childs from EOS device tree
+eos_error_t eos_dev_detach(eos_dev_t *dev);
