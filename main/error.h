074a74db2dfcd64bedd509ac475f95468134db77
Alex <15167344+frostmorn@users.noreply.github.com>
2026-07-02 17:13:26 +0300

    update eos error codes list

--- main/error.h
+++ main/error.h
@@ -3,7 +3,14 @@
 // EOS Project header file
 ///////////////////////////////////////////////////////
 
-typedef enum { EOS_NO_ERROR, EOS_NO_MEM_LEFT_ERROR } eos_error_t;
+typedef enum {
+  EOS_NO_ERROR,
+  EOS_NO_MEM_LEFT_ERROR,
+  EOS_DEVICE_COUNT_QUOTA_EXCEED,
+  EOS_DEVICE_BUS_INVALID,
+  EOS_DEVICE_INVALID,
+  EOS_DEVICE_ALREADY_ATTACHED
+} eos_error_t;
 
 extern eos_error_t eos_errno;
 
