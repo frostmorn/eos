1403a1d2e720e3e7dd5d1b8e0a9062a9d7a4ea04
Alex <15167344+frostmorn@users.noreply.github.com>
2026-07-05 03:37:07 +0300

    reserve space for board implementations

---
+++ main/board/board.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "sys/devtree.h"
+
+// Per board specific initialization
+extern void board_init();
---
+++ main/board/lilka_v23.c
@@ -0,0 +1,11 @@
+#include "pregen.h"
+#ifdef EOS_TARGET_LILKAV2
+#include "board.h"
+void board_init() {
+  eos_dev_t *spi = eos_dev_alloc();
+  //   spi->driver = EOS_DRIVER(bus, spi);
+  //   spi->config = EOS_DRIVER_CONFIG(bus, spi, .mosi = 11, .miso = 13, .sck =
+  //   12);
+  eos_dev_attach(spi, eos_devtree_root());
+}
+#endif
