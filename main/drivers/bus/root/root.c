3b3086c913cfd711f2a1c6ac7cd99f6d340f739b
Alex <15167344+frostmorn@users.noreply.github.com>
2026-07-10 00:29:41 +0300

    Accept all ATTACH/DETACH requests for root bus

--- main/drivers/bus/root/root.c
+++ main/drivers/bus/root/root.c
@@ -1,17 +1,35 @@
-#include "sys/driver.h"
+#include "drivers/bus/bus.h"
 #include "includes.h"
-
+#include "sys/driver.h"
 // This driver is always enabled,
 // and represents root bus which is an origin point of our device tree
 
-int driver_bus_root_init(eos_dev_t *dev) { return 0;}
+int driver_bus_root_init(eos_dev_t *dev_bus) { return 0; }
 
-int driver_bus_root_read(eos_dev_t *dev, void *buf, size_t len) {return 0;}
+int driver_bus_root_read(eos_dev_t *dev_bus, void *buf, size_t len) {
+  return 0;
+}
 
-int driver_bus_root_write(eos_dev_t *dev, void *buf, size_t len) {return 0;}
+int driver_bus_root_write(eos_dev_t *dev_bus, void *buf, size_t len) {
+  return 0;
+}
 
-int driver_bus_root_ioctl(eos_dev_t *dev, int cmd, va_list args) {return 0;}
+int driver_bus_root_ioctl(eos_dev_t *dev_bus, int cmd, va_list args) {
+  switch (cmd) {
+    // ROOT BUS always accepts all ATTACH/DETACH requests
+  case EOS_BUS_IOCTL_KID_ATTACH: {
+    return true;
+    break;
+  }
+  case EOS_BUS_IOCTL_KID_DETACH: {
+    return true;
+    break;
+  }
+  }
 
-void driver_bus_root_shutdown(eos_dev_t *dev) {}
+  return 0;
+}
+
+void driver_bus_root_shutdown(eos_dev_t *dev_bus) {}
 
 EOS_DRIVER_REG(bus, root, EOS_INIT_DRIVERS_ROOT_BUS);
