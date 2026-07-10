c052f79e3ccf9e2c4e7097ea378d5a3478e28bfd
Alex <15167344+frostmorn@users.noreply.github.com>
2026-07-10 23:37:04 +0300

    assert if driver not found in a system

--- main/misc/fancymacro.h
+++ main/misc/fancymacro.h
@@ -1,4 +1,6 @@
 #pragma once
-
+#include <esp_log.h>
 #define EOS_STR_HELPER(x) #x
 #define EOS_STR(x) EOS_STR_HELPER(x)
+
+#define EOS_LOGE(format, ...) ESP_LOGE(__FILE__, format, ##__VA_ARGS__)
--- main/sys/driver.c
+++ main/sys/driver.c
@@ -1,4 +1,5 @@
 #include "sys/driver.h"
+#include "misc/fancymacro.h"
 
 uint32_t eos_drivers_count = 0;
 eos_driver_t eos_drivers[EOS_MAX_DRIVERS];
@@ -9,5 +10,7 @@ eos_driver_t *eos_driver_find(char *scope, char *name) {
         (strcmp(name, eos_drivers[i].name) == 0))
       return &eos_drivers[i];
 
+  EOS_LOGE("Not found driver for %s/%s\n", scope, name);
+  assert(0);
   return NULL;
 }
