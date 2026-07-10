35f8d33482fb1d22ce2b38bf2dc96c306179d9b7
Alex <15167344+frostmorn@users.noreply.github.com>
2026-07-10 19:47:52 +0300

    eos_error_to_str implementation

--- main/sys/error.c
+++ main/sys/error.c
@@ -1,3 +1,20 @@
 #include "error.h"
 
 eos_error_t eos_errno = EOS_NO_ERROR;
+
+static const char *eos_error_strings[] = {
+    [EOS_NO_ERROR] = "no error",
+    [EOS_NO_MEM_LEFT_ERROR] = "no memory left",
+    [EOS_DEVICE_COUNT_QUOTA_EXCEED] = "device count quota exceeded",
+    [EOS_DEVICE_BUS_INVALID] = "bus device invalid",
+    [EOS_DEVICE_INVALID] = "device invalid",
+    [EOS_DEVICE_ALREADY_ATTACHED] = "device already attached",
+    [EOS_DEVICE_ATTACH_DECLINED_BY_BUS] = "attach declined by bus",
+    [EOS_DEVICE_DETACH_DECLINED_BY_BUS] = "detach declined by bus",
+};
+
+char *eos_error_to_str(eos_error_t err) {
+  if (err >= sizeof(eos_error_strings) / sizeof(*eos_error_strings))
+    return "unknown error";
+  return (char *)eos_error_strings[err];
+}
