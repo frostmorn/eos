d524a4e6813f74f6a7542b9a2e4c00d8349afb19
Alex <15167344+frostmorn@users.noreply.github.com>
2026-07-13 04:53:53 +0300

    implement eos_cfg_get_ helpers

--- main/misc/types.h
+++ main/misc/types.h
@@ -34,3 +34,42 @@ static inline int32_t eos_pin_get_no(eos_pin_t *pins, const char *key) {
       return p->val;
   return -1; // unused in esp-idf
 }
+
+static inline eos_cfg_t *eos_cfg_get(eos_cfg_t *cfg, const char *key) {
+  if (!cfg)
+    return NULL;
+  for (eos_cfg_t *c = cfg; c->key != NULL; c++)
+    if (strcmp(c->key, key) == 0)
+      return c;
+  return NULL;
+}
+
+static inline int32_t eos_cfg_get_i(eos_cfg_t *cfg, const char *key,
+                                    int32_t def) {
+  eos_cfg_t *e = eos_cfg_get(cfg, key);
+  if (!e || e->type != EOS_CFG_INT)
+    return def;
+  return e->val.i;
+}
+
+static inline float eos_cfg_get_f(eos_cfg_t *cfg, const char *key, float def) {
+  eos_cfg_t *e = eos_cfg_get(cfg, key);
+  if (!e || e->type != EOS_CFG_FLOAT)
+    return def;
+  return e->val.f;
+}
+
+static inline char *eos_cfg_get_s(eos_cfg_t *cfg, const char *key, char *def) {
+  eos_cfg_t *e = eos_cfg_get(cfg, key);
+  if (!e || e->type != EOS_CFG_STR)
+    return def;
+  return e->val.s;
+}
+
+static inline void *eos_cfg_get_ptr(eos_cfg_t *cfg, const char *key,
+                                    void *def) {
+  eos_cfg_t *e = eos_cfg_get(cfg, key);
+  if (!e || e->type != EOS_CFG_PTR)
+    return def;
+  return e->val.ptr;
+}
