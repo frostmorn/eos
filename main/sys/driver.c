43e7c665961a547c172dd989123fdc5187fb4578
Alex <15167344+frostmorn@users.noreply.github.com>
2026-07-13 09:07:00 +0300

    Add some debug to eos_driver_find. allocate eso_drivers structure in external memory

--- main/sys/driver.c
+++ main/sys/driver.c
@@ -2,15 +2,18 @@
 #include "misc/fancymacro.h"
 
 uint32_t eos_drivers_count = 0;
-eos_driver_t eos_drivers[EOS_MAX_DRIVERS];
+EXT_RAM_BSS_ATTR eos_driver_t eos_drivers[EOS_MAX_DRIVERS];
 
 eos_driver_t *eos_driver_find(char *scope, char *name) {
+
   for (int i = 0; i < eos_drivers_count; i++)
     if ((strcmp(scope, eos_drivers[i].scope) == 0) &&
-        (strcmp(name, eos_drivers[i].name) == 0))
+        (strcmp(name, eos_drivers[i].name) == 0)) {
+      EOS_LOGI("Found driver %s/%s", scope, name);
       return &eos_drivers[i];
+    }
 
-  EOS_LOGE("Not found driver for %s/%s\n", scope, name);
+  EOS_LOGE("Not found driver for %s/%s", scope, name);
   assert(0);
   return NULL;
 }
