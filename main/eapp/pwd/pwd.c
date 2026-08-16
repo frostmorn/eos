4178c8ed96ce5eff149ff80f25ba3bb415716ad5
Alex <15167344+frostmorn@users.noreply.github.com>
2026-08-16 07:29:30 +0000

    fixes to pwd

--- main/eapp/pwd/pwd.c
+++ main/eapp/pwd/pwd.c
@@ -1,5 +1,6 @@
 #include "ecore/app.h"
 #include "ecore/unistd_ext.h"
+#include <stdio.h>
 
 int pwd_main(int argc, char **argv){
   // getcwd_fast never fails
@@ -7,6 +8,6 @@ int pwd_main(int argc, char **argv){
   return 0;
 }
 
-EOS_NATIVE_APP_ATTR eos_native_app_manifest_t tree_app = {
+EOS_NATIVE_APP_ATTR eos_native_app_manifest_t pwd_app = {
     EOS_NATIVE_APP_INIT, .filename = "pwd", .name = "pwd",
     .entry_point = pwd_main};
